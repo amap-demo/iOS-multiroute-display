9e76095fab12cdafdcfbd177514d2a8800c955de
liubo.liub <liubo.liub@alibaba-inc.com>
2016-12-23 11:55:04 +0800

    add key

--- MultiRoutePlan/Configuration/APIKey.h
+++ MultiRoutePlan/Configuration/APIKey.h
@@ -11,6 +11,6 @@
 
 /* 使用高德地图API，请注册Key，注册地址：http://lbs.amap.com/console/key */
 
-const static NSString *APIKey = @"";
+const static NSString *APIKey = @"0f79639ff086b3a0ec997c3dcd7236a6";
 
 #endif
